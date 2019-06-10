66f7efabcc42d820cdd08e6914dee9e672e62612
Matt Rees <matthew.rees@ee.co.uk>
2019-06-10 16:01:29 +0100

    Added missing import

--- remote_control/remote_control/path.py
+++ remote_control/remote_control/path.py
@@ -1,6 +1,7 @@
 import time
 from picar import back_wheels, front_wheels
 
+db_file = "/home/pi/SunFounder_PiCar-V/remote_control/remote_control/driver/config"
 fw = front_wheels.Front_Wheels(debug=False, db=db_file)
 bw = back_wheels.Back_Wheels(debug=False, db=db_file)
 
